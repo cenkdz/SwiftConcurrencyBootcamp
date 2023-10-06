c314f8f3276eb65a7c7924220571287b4d1dba2a
Cenk Donmez <56077278+cenkdz@users.noreply.github.com>
2023-10-06 19:30:29 +0300

    First

---
+++ ConcurrencyBootcamp/ConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyBootcampApp.swift
+//  ConcurrencyBootcamp
+//
+//  Created by Cenk Donmez on 6.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcurrencyBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConcurrencyBootcamp
+//
+//  Created by Cenk Donmez on 6.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
